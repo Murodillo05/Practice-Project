16f19f00423ff35ca619eebf223c2feb18b535f7
Abduvakhab <abduvohobbohodirov777@gmail.com>
2024-09-04 14:58:47 +0500

    added new model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Discounts(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    
+    back_image = models.ImageField(upload_to="Image")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    finished_at = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.name
