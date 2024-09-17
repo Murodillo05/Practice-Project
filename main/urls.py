e1e637646a071363305b50dba60ef541bbff0c20
Bexruz777-ux <mamirzonovlegenda@gmail.com>
2024-09-17 19:41:30 +0500

    added urls

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('program/', views.program, name='program'),
     path('questions/', views.questions, name='questions'),
     path('testimonal/', views.testimonal, name='testimonal'),
-    
+    path('apartments/', views.apartments, name='apartments'),
+    path('apartments_info/', views.apartmentsinfo, name='apartments_info')
 ]
